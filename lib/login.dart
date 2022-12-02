8b8fae352ebbd0c21274143ef549b8b5cae3e2d8
Luiz Felipe <lfar05071@gmail.com>
2022-12-02 20:55:59 -0300

    tela login

--- lib/login.dart
+++ lib/login.dart
@@ -2,65 +2,102 @@ import 'package:flutter/material.dart';
 import 'Cadastros/cadastro.dart';
 
 class login extends StatefulWidget {
+  const login({Key? key}) : super(key: key);
+
   @override
-  _loginState createState() => _loginState();
+  State<login> createState() => _loginState();
 }
 
 class _loginState extends State<login>{
 
-  Widget _body(){
-   return SingleChildScrollView(
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: ListView(
-        children: [
-          Container(
-        height: 30,
-        ),
-          Container(
-            child: Column(children: [
-              ElevatedButton(
-                child: Text(
-                  "CADASTRAR",
-                  style: TextStyle(color: Colors.white, fontSize: 18),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    shadowColor: Colors.black,
-                    elevation: 10,
-                    padding: EdgeInsets.symmetric(horizontal: 90.0, vertical: 16.0),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    primary: Color.fromRGBO(73, 39, 196, 1)
-                  ),
-                    onPressed: () {
-                      Navigator.of(context).pushReplacementNamed ('/cadastro');
-                    },
-                  )
-                ]
-              ),
-            )           
-          ],
-        ),
-      )
-    );
-  } 
-
-
-@override
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          Container(  
-          decoration: BoxDecoration(
-            color: Colors.white
-          ), 
-        ),
-          _body(),
-        ],
-      )
+    return MaterialApp(
+      home: Scaffold(
+        body: const MyStatefulWidget(),
+      ),
     );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(50),
+        child: ListView(
+          children: <Widget>[
+            Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                margin: const EdgeInsets.fromLTRB(0, 35, 0, 35),
+                child: Image.asset('assets/couscous.png')
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  suffixIcon: Icon(Icons.person),
+                  labelText: 'Email',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+              child: TextField(
+                obscureText: true,
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  suffixIcon: Icon(Icons.lock_open),
+                  labelText: 'Senha',
+                ),
+              ),
+            ),
+            Container(
+                height: 50,
+                padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                margin: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                child: ElevatedButton(
+                  child: const Text('Login'),
+                  onPressed: () {
+                  },
+                  style: TextButton.styleFrom(
+                    // shape: ,
+                    backgroundColor: Color.fromARGB(255, 73, 39, 196),
+                  ),
+                )
+            ),Container(
+                height: 50,
+                margin: const EdgeInsets.fromLTRB(0, 0, 0, 15),
+                padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                child: ElevatedButton(
+                  child: const Text('Cadastro'),
+                  onPressed: () {
+                  },
+                  style: TextButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 73, 39, 196)
+                  ),
+                )
+            ),
+            TextButton(
+              onPressed: () {
+              },
+              child: const Text('Esqueci a senha'),
+            ),
+          ],
+        ));
   }
 }
